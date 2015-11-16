bc1e2b0b710cf1f78477ee4dde4f05f1a5f569ab
Ryo Yonetani <yonetani@iis.u-tokyo.ac.jp>
2015-11-16 17:11:51 +0900

    Add link

---
+++ link.sh
@@ -0,0 +1,7 @@
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.profile ~/.profile
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.profile ~/.profile
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
